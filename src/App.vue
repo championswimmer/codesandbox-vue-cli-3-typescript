3c4bdcdcd7feb6edc1aabeae04c3301a022ab3de
Bogdan Luca <luca.bogdan@gmail.com>
2018-12-20 19:23:50 +0200

    Changed message.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Vue CLI 3 in <a href='https://hackernoon.com/codesandbox-containers-5864a8f26715' target='_blank'>CodeSandbox Containers"/>
+    <HelloWorld msg="Vue CLI 3 in <a href='https://hackernoon.com/codesandbox-containers-5864a8f26715' target='_blank'>CodeSandbox Containers!"/>
   </div>
 </template>
 
