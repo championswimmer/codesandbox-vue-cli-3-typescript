f467f3aeed85e878c6b5ddc433a7271c49435a19
Bogdan Luca <luca.bogdan@gmail.com>
2018-12-20 19:43:59 +0200

    Upgraded vue packages, added cuid.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1 v-html="msg"/>
+    <h2>{{ cuid() }}</h2>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -31,10 +32,15 @@
 </template>
 
 <script>
+import cuid from 'cuid';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+    cuid
   }
 }
 </script>
